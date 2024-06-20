7fe69bb26d539908910c58601a369834b1099ace
${GITHUB_USER} <bruno@novo.com>
2024-06-20 17:59:03 -0300

    :pencil: Revisão ao Vivo VUEJS

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import TodoList from './components/TodoList.vue';
+</script>
+
+<template>
+  <div id="app">
+    <TodoList />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #82acd5;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { ref } from 'vue';
+
+const newTask = ref('');
+const tasks = ref([]);
+
+function addTask() {
+  if (newTask.value.trim()) {
+    tasks.value.push(newTask.value.trim());
+    newTask.value = '';
+  }
+}
+
+function removeTask(index) {
+  tasks.value.splice(index, 1);
+}
+</script>
+
+<template>
+    <div class="todo-list">
+      <h1>Lista de Tarefas</h1>
+      <input v-model="newTask" @keyup.enter="addTask" placeholder="Adicionar nova tarefa" />
+      <button @click="addTask">Adicionar</button>
+      <ul>
+        <li v-for="(task, index) in tasks" :key="index">
+          {{ task }}
+          <button @click="removeTask(index)">Remover</button>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <style scoped>
+  .todo-list {
+    max-width: 400px;
+    margin: 0 auto;
+    text-align: center;
+  }
+  input {
+    width: calc(100% - 22px);
+    padding: 10px;
+    margin-bottom: 10px;
+  }
+  button {
+    margin-left: 10px;
+    padding: 10px;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 5px;
+  }
+  </style>
+  
